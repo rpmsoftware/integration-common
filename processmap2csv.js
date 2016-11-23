8ba92f102a8bb5ae81927e881efd8838fc6359c3
Dmitriy Serdyukov <dmitriy@rpmsoftware.com>
2016-11-23 15:02:42 -0700

    Introduced global dependencies

--- processmap2csv.js
+++ processmap2csv.js
@@ -1,10 +1,8 @@
-/* global process */
-(function () {
-    var promised = require('promised-io/promise');
-    var Deferred = promised.Deferred;
+(() => {
     var rpm = require('./api-wrappers');
-    var api = new rpm.RpmApi(require('./util').readConfig('RPM_CONFIG', 'config.json'));
+    var api = new rpm.RpmApi(require('./util').readConfig('RPM_CONFIG', 'config.json').rpmApiMagnum);
     var util = require('util');
+    var assert = require('assert');
 
     var reffieldType = rpm.OBJECT_TYPE.FormReference;
     var refSubTypes = {};
@@ -12,85 +10,58 @@
         refSubTypes[rpm.REF_DATA_TYPE[key]] = key;
     }
 
-    function getFields(proc) {
-        processes[proc.ProcessID] = proc;
-        var deferred = new Deferred();
-        proc.getFields().then(
-            function (result) {
-                deferred.resolve(result);
-            },
-            function (error) {
-                console.error(util.format('Cannot get fields for process "%s" (%d)', proc.Process, proc.ProcessID), error);
-                deferred.resolve();
-            });
-        return deferred.promise;
-    }
 
     var processes = {};
-
-    promised.seq([
-        function () {
-            return api.getProcesses(true);
-        },
-        function (response) {
+    var data = [];
+    var fieldsByUid = {};
+    
+    api.getProcesses(true)
+        .then(response => {
+            processes = response.toObject('ProcessID');
             if (!Array.isArray(response)) {
                 response = [response];
             }
-            var steps = [];
-            var data = [];
-            var fieldsByUid = {};
+            var p = Promise.resolve();
 
-            response.forEach(function (proc) {
-                steps.push(function () {
-                    return getFields(proc);
-                });
-                steps.push(function (fields) {
-                    if (!fields) {
-                        return;
-                    }
-
-                    var process = processes[fields.ProcessID];
-                    fields.Fields.forEach(function (field) {
+            response.forEach(proc => p = p
+                .then(() => proc.getFields())
+                .then(fields => {
+                    fields.Fields.forEach(field => {
 
                         if (field.FieldType !== reffieldType) {
                             return;
                         }
-                        field.process = process;
+                        field.process = processes[fields.ProcessID];
+                        assert(field.process);
                         fieldsByUid[field.Uid] = field;
                         data.push(field);
                     });
-                });
-            });
-            steps.push(function () {
-                return data.map(function (field) {
-                    var refProcName = processes[field.ProcessID];
-                    refProcName = refProcName && refProcName.Process || util.format('[%s]', refSubTypes[field.SubType]);
-                    var parent = fieldsByUid[field.ParentUid];
-                    return {
-                        'Process ID': field.process.ProcessID,
-                        'Process Name': field.process.Process,
-                        'Process Enabled': field.process.Enabled,
-                        'Field Uid': field.Uid,
-                        'Field Name': field.Name,
-                        'Field Archived': field.Archived,
-                        'Parent Process ID': parent && parent.process.ProcessID,
-                        'Parent Process Name': parent && parent.process.Process,
-                        'Parent Uid': field.ParentUid,
-                        'Parent Name': parent && parent.Name,
-                        'Ref Process ID': field.ProcessID,
-                        'Ref Process Name': refProcName
-                    };
-                });
-            });
-            return promised.seq(steps);
-        }
-    ]).then(
-        function (result) {
+                })
+            );
+            return p;
+        })
+        .then(() => data.map(field => {
+            var refProcName = processes[field.ProcessID];
+            refProcName = refProcName && refProcName.Process || util.format('[%s]', refSubTypes[field.SubType]);
+            var parent = fieldsByUid[field.ParentUid];
+            return {
+                'Process ID': field.process.ProcessID,
+                'Process Name': field.process.Process,
+                'Process Enabled': field.process.Enabled,
+                'Field Uid': field.Uid,
+                'Field Name': field.Name,
+                'Field Archived': field.Archived,
+                'Parent Process ID': parent && parent.process.ProcessID,
+                'Parent Process Name': parent && parent.process.Process,
+                'Parent Uid': field.ParentUid,
+                'Parent Name': parent && parent.Name,
+                'Ref Process ID': field.ProcessID,
+                'Ref Process Name': refProcName
+            };
+        }))
+        .then(result => {
             result = require('to-csv')(result);
             var fileName = process.argv[2];
             fileName ? require('fs').writeFileSync(fileName, result) : console.info(result);
-        },
-        function (error) {
-            console.error(error);
-        });
+        }, console.error);
 })();
