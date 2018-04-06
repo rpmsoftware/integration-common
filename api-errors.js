e4b597e54ff7604bc5f2657871fb4c0aade4a036
Dmitriy Serdyukov <dmitriy@rpmsoftware.com>
2018-04-06 11:07:53 -0600

    Added 'Supplier not found' to errors

--- api-errors.js
+++ api-errors.js
@@ -2,3 +2,4 @@ exports.MSG_CUSTOMER_NOT_FOUND = 'Customer not found';
 exports.MSG_AGENCY_GROUP_NOT_FOUND = 'Agency group not found';
 exports.MSG_AGENCY_NOT_FOUND = 'Agency not found';
 exports.MSG_ACCOUNT_NOT_FOUND = 'Account not found';
+exports.MSG_SUPPLIER_NOT_FOUND = 'Supplier not found';
