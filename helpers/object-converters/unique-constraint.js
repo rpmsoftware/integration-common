5060bcf2828c7e0fa29620e06cbeb537562e9ab2
Dmitriy Serdyukov <dmitriy@rpmsoftware.com>
2023-03-31 16:02:02 -0600

    Add duplicates removal for unique-constraint converter

--- helpers/object-converters/unique-constraint.js
+++ helpers/object-converters/unique-constraint.js
@@ -3,38 +3,45 @@ const assert = require('assert');
 const hash = require('object-hash');
 
 module.exports = {
-    init: function ({ properties, ignoreEmpty }) {
+    init: function ({ properties, ignoreEmpty, removeDuplicates }) {
         ignoreEmpty = toBoolean(ignoreEmpty) || undefined;
+        removeDuplicates = toBoolean(removeDuplicates) || undefined;
         properties = toArray(properties);
         assert(properties.length > 0);
         properties = properties.map(validatePropertyConfig);
-        return { properties, ignoreEmpty };
+        return { properties, ignoreEmpty, removeDuplicates };
     },
-    convert: function ({ properties, ignoreEmpty }, obj) {
-        const uniqueObjects = {};
+    convert: function ({ properties, ignoreEmpty, removeDuplicates }, obj) {
+        const objectsByHash = {};
         let duplicates = {};
-        for (const e of toArray(obj)) {
-            let empty = true;
-            const values = properties.map(p => {
-                const r = getDeepValue(e, p);
-                empty = empty && (r === undefined || r === null || r === '');
-                return r;
-            });
-
-            if (empty && ignoreEmpty) {
-
-                continue;
+        if (Array.isArray(obj)) {
+            for (const e of obj) {
+                let empty = true;
+                const values = properties.map(p => {
+                    const r = getDeepValue(e, p);
+                    empty = empty && (r === undefined || r === null || r === '');
+                    return r;
+                });
+                if (empty && ignoreEmpty) {
+                    continue;
+                }
+                const h = hash(values);
+                const d = objectsByHash[h] || (objectsByHash[h] = []);
+                d.push(e);
+                d.length > 1 && !duplicates[h] && (duplicates[h] = values);
             }
-            const h = hash(values);
-            const d = uniqueObjects[h];
-            d ?
-                (duplicates[h] || (duplicates[h] = values)) :
-                (uniqueObjects[h] = true);
-
-        }
-        if (!isEmpty(duplicates)) {
-            duplicates = Object.values(duplicates).map(values => values.join(',')).join('\n');
-            throw new Error(`Duplicate entries found:\n${duplicates}`);
+            if (isEmpty(duplicates)) {
+                // do nothing
+            } else if (removeDuplicates) {
+                obj = [];
+                for (let o in objectsByHash) {
+                    o = objectsByHash[o];
+                    o.length < 2 && (obj = obj.concat(o));
+                }
+            } else {
+                duplicates = Object.values(duplicates).map(values => values.join(',')).join('\n');
+                throw new Error(`Duplicate entries found:\n${duplicates}`);
+            }
         }
         return obj;
     }
