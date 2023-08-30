63b92f1288829d34194aeabc5e96a88021164b1f
Dmitriy Serdyukov <dmitriy@rpmsoftware.com>
2023-08-30 08:44:33 -0600

    Add fixed staff groups to get-staff-stubs processor

--- helpers/object-converters/get-staff-stubs.js
+++ helpers/object-converters/get-staff-stubs.js
@@ -1,3 +1,4 @@
+const assert = require('assert');
 const { validateString, toArray, validatePropertyConfig, getDeepValue } = require('../../util');
 
 const getStaffList = async function () {
@@ -12,20 +13,36 @@ const getStaffList = async function () {
 module.exports = {
     init: async function ({ dstProperty, groups }) {
         validateString(dstProperty);
-        groups = groups ? validatePropertyConfig(groups) : undefined;
+        if (groups || (groups = undefined)) {
+            let { fixed, property } = typeof groups === 'string' ? { property: groups } : groups;
+            if (fixed || (fixed = undefined)) {
+                const { Groups } = await this.api.getStaffGroups();
+                fixed = toArray(fixed).map(nameOrID => Groups.demand(({ ID, Group }) => Group === nameOrID || ID === nameOrID).ID);
+                assert(fixed.length > 0);
+            } else {
+                property = validatePropertyConfig(property || groups)
+            }
+            groups = { fixed, property };
+        }
         return { dstProperty, groups };
     },
 
     convert: async function ({ dstProperty, groups: groupsCfg }, data) {
-        const { StaffList } = await getStaffList.call(this.api);
+        let { StaffList } = await getStaffList.call(this.api);
+
+        const { fixed, property } = groupsCfg || {};
+        fixed && (StaffList = StaffList.filter(({ Groups }) => Groups.find(({ ID }) => fixed.indexOf(ID) >= 0)));
+
         for (const e of toArray(data)) {
             let result = StaffList;
-            let groups = getDeepValue(e, groupsCfg);
-            if (groups) {
-                groups = toArray(groups);
-                result = result.filter(({ Groups }) => Groups.find(({ Group, ID }) =>
-                    groups.find(nameOrID => nameOrID === Group || nameOrID === ID)
-                ));
+            if (property) {
+                let groups = getDeepValue(e, property);
+                if (groups) {
+                    groups = toArray(groups);
+                    result = result.filter(({ Groups }) => Groups.find(({ Group, ID }) =>
+                        groups.find(nameOrID => nameOrID === Group || nameOrID === ID)
+                    ));
+                }
             }
             e[dstProperty] = result;
         }
