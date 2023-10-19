78f0e2a47a6ac0bb342f9cb70b50bdd75f4873ca
Dmitriy Serdyukov <dmitriy@rpmsoftware.com>
2023-10-19 14:27:32 -0600

    Add "rpm/api-method" converter

---
+++ helpers/object-converters/rpm/api-method.js
@@ -0,0 +1,40 @@
+const { validateString, toArray, validatePropertyConfig, getDeepValue } = require('../../../util');
+const { init: initCondition, process: processCondition } = require('../../../conditions');
+const assert = require('assert');
+
+module.exports = {
+    init: function ({ dstProperty, parameters, condition, method }) {
+        validateString(method);
+        validateString(dstProperty);
+        parameters = toArray(parameters);
+        parameters = parameters.map(c => {
+            let { value, property } = c;
+            if (value !== undefined) {
+                property = undefined;
+            } else if (!property) {
+                property = c;
+            }
+            if (property) {
+                value = undefined;
+                property = validatePropertyConfig(property);
+            } else {
+                assert(value !== undefined);
+            }
+            return { value, property };
+        });
+        condition = condition ? initCondition(condition) : undefined;
+        return { dstProperty, parameters, condition, method };
+    },
+
+    convert: async function ({ method, dstProperty, parameters, condition }, obj) {
+        const { api } = this;
+        for (const e of toArray(obj)) {
+            (!condition || processCondition(condition, e)) && (
+                e[dstProperty] = await api[method].apply(api, parameters.map(
+                    ({ value, property }) => value === undefined ? getDeepValue(e, property) : value)
+                )
+            );
+        }
+        return obj;
+    }
+};
