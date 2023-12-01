660c6680d6559628854a978035f790ed4d895ab2
Dmitriy Serdyukov <dmitriy@rpmsoftware.com>
2023-12-01 11:18:37 -0700

    Add merging for "method" converter

--- helpers/object-converters/method.js
+++ helpers/object-converters/method.js
@@ -1,19 +1,20 @@
 const { propertyOrValue } = require('./util');
-const { validateString, toArray } = require('../../util');
+const { validateString, toArray, toBoolean } = require('../../util');
 const { init: initCondition, process: processCondition } = require('../../conditions');
 
 module.exports = {
-    init: function ({ srcProperty, dstProperty, parameters, condition, method }) {
+    init: function ({ srcProperty, dstProperty, parameters, condition, method, merge }) {
         validateString(method);
         dstProperty = dstProperty ? validateString(dstProperty) : undefined;
         srcProperty = srcProperty ? validateString(srcProperty) : undefined;
         parameters = toArray(parameters);
         parameters = parameters.map(propertyOrValue.init);
         condition = condition ? initCondition(condition) : undefined;
-        return { srcProperty, dstProperty, parameters, condition, method };
+        merge = toBoolean(merge) || undefined;
+        return { srcProperty, dstProperty, parameters, condition, method, merge };
     },
 
-    convert: async function ({ method, dstProperty, parameters, condition, getMethodContext }, obj) {
+    convert: async function ({ method, dstProperty, parameters, condition, getMethodContext, merge }, obj) {
         (typeof getMethodContext === 'function') || (getMethodContext = e => e);
         for (const e of toArray(obj)) {
             if (condition && !processCondition(condition, e)) {
@@ -21,7 +22,7 @@ module.exports = {
             }
             const context = await getMethodContext(e);
             const r = await context[method].apply(context, parameters.map(c => propertyOrValue.get(c, e)));
-            dstProperty && (e[dstProperty] = r);
+            dstProperty ? (e[dstProperty] = r) : (merge && Object.assign(e, r));
         }
         return obj;
     }
