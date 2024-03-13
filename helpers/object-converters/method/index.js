06e431e2d6b350b690dde6867bbfeed99e191eca
Dmitriy Serdyukov <dmitriy@rpmsoftware.com>
2024-03-13 16:16:24 -0600

    Add errorProperty to method converter

--- helpers/object-converters/method/index.js
+++ helpers/object-converters/method/index.js
@@ -39,7 +39,8 @@ const getInstances = () => {
 };
 
 module.exports = {
-    init: function ({ srcProperty, dstProperty, parameters, condition, method, merge, contextFactory }) {
+    init: function ({ srcProperty, dstProperty, parameters, condition, method, merge, contextFactory, errorProperty }) {
+        errorProperty = errorProperty ? validateString(errorProperty) : undefined;
         method = method ? validateString(method) : undefined;
         dstProperty = dstProperty ? validateString(dstProperty) : undefined;
         srcProperty = srcProperty ? validateString(srcProperty) : undefined;
@@ -47,11 +48,12 @@ module.exports = {
         condition = condition ? initCondition(condition) : undefined;
         merge = toBoolean(merge) || undefined;
         contextFactory ? getEager(getGlobal(), validateString(contextFactory)) : (contextFactory = undefined);
-        return { srcProperty, dstProperty, parameters, condition, method, merge, contextFactory };
+        return { srcProperty, dstProperty, parameters, condition, method, merge, contextFactory, errorProperty };
     },
 
     convert: async function (conf, obj) {
-        let { method, dstProperty, parameters: paramConf, condition, getMethodContext, merge, contextFactory } = conf;
+        let { method, dstProperty, parameters: paramConf, condition,
+            getMethodContext, merge, contextFactory, errorProperty } = conf;
         if (getMethodContext !== 'function') {
             if (contextFactory) {
                 const instances = getInstances();
@@ -73,8 +75,16 @@ module.exports = {
             }
             const context = await getMethodContext(e);
             const params = paramConf.map(c => extractParameter(c, e));
-            const r = await (method ? context[method].apply(context, params) : context.apply(undefined, params));
-            dstProperty ? (e[dstProperty] = r) : (merge && Object.assign(e, r));
+            try {
+                const r = await (method ? context[method].apply(context, params) : context.apply(undefined, params));
+                errorProperty && delete e[errorProperty];
+                dstProperty ? (e[dstProperty] = r) : (merge && Object.assign(e, r));
+            } catch (error) {
+                if (!errorProperty) {
+                    throw error;
+                }
+                e[errorProperty] = error;
+            }
         }
         return obj;
     }
